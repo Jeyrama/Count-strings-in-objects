27f51bbfed7a3cdf1cec168d6abea853814c4ffa
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-29 11:39:57 -0700

    Update Count-strings-in-objects.js

--- Count-strings-in-objects.js
+++ Count-strings-in-objects.js
@@ -15,3 +15,12 @@ For example:
 
 
 // Solution
+
+function strCount(obj){
+  let count = 0;
+  for (key in obj) {
+    if (typeof obj[key] == 'string') count++;
+    if (typeof obj[key] == 'object') count += strCount(obj[key]);
+  }
+  return count;
+}
